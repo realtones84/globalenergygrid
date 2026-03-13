4b6d51e24ebd93a867a7b6de6f06118ff42a36f0
realtones84 <globalenergygrid@gmail.com>
2026-03-13 05:14:09 -0400

    Create phiNodes.js

---
+++ phi-earth-explorer/phiNodes.js
@@ -0,0 +1,39 @@
+const phiNodes = [
+
+{name:"Giza",lat:29.9792,lng:31.1342,phi:3},
+
+{name:"Jerusalem",lat:31.7779,lng:35.2358,phi:4},
+
+{name:"Göbekli Tepe",lat:37.2231,lng:38.9225,phi:1},
+
+{name:"Stonehenge",lat:51.1788,lng:-1.8262,phi:7},
+
+{name:"Newgrange",lat:53.6946,lng:-6.4756,phi:8},
+
+{name:"Rosslyn Chapel",lat:55.8565,lng:-3.1590,phi:9},
+
+{name:"Teotihuacan",lat:19.6925,lng:-98.8438,phi:2},
+
+{name:"Chichen Itza",lat:20.6830,lng:-88.5686,phi:2},
+
+{name:"Monte Alban",lat:17.0431,lng:-96.7675,phi:2},
+
+{name:"Cuicuilco",lat:19.3256,lng:-99.1750,phi:2},
+
+{name:"Tikal",lat:17.2221,lng:-89.6232,phi:2},
+
+{name:"Machu Picchu",lat:-13.1631,lng:-72.5450,phi:7},
+
+{name:"Yucay Valley",lat:-13.2658,lng:-72.1833,phi:2},
+
+{name:"Great Zimbabwe",lat:-20.2668,lng:28.6112,phi:2},
+
+{name:"Angkor Wat",lat:13.4125,lng:103.8669,phi:2},
+
+{name:"Gunung Padang",lat:-6.9931,lng:107.0450,phi:1},
+
+{name:"Derinkuyu",lat:38.3730,lng:34.7416,phi:10},
+
+{name:"Hampi",lat:15.3350,lng:76.4600,phi:2}
+
+]
